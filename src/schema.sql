2b5e1d845e9b9726a375d6caff02effc77b2ef23
Spyros Roum <spyrosr1@gmail.com>
2020-05-07 13:33:45 +0300

    Add db schema

---
+++ src/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS settings (
+    bot_token TEXT,
+    CONSTRAINT token UNIQUE (bot_token)
+);
+
+CREATE TABLE IF NOT EXISTS words (
+    g_id NUMERIC NOT NULL,
+    chn_id NUMERIC NOT NULL,
+    word TEXT,
+    file_path TEXT NOT NULL,
+    CONSTRAINT u_g_chn_word UNIQUE  ( g_id, chn_id, word )
+);
