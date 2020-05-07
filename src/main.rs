8aa577ed835d8402e3fdcc825f8e96098c19a0a7
Spyros Roum <spyrosr1@gmail.com>
2020-05-07 14:03:14 +0300

    Create db and tables if not exists

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+
+use sqlx;
+use sqlx::SqlitePool;
+
+use async_std::task;
+use std::io::Read;
+
+async fn create_sqlite_pool() -> Result<SqlitePool, sqlx::Error> {
+    SqlitePool::new("sqlite://DATA/app.db").await
+}
+
+async fn create_db_tables(pool: &SqlitePool) {
+    let mut file = File::open("DATA/schema.sql").expect("Failed to open schema file.");
+    let mut schema = String::new();
+    file.read_to_string(&mut schema).unwrap();
+
+    sqlx::query(&schema)
+        .execute(pool)
+        .await
+        .expect("Failed to create tables");
+}
+
+fn main() -> Result<(), sqlx::Error> {
+    let pool = task::block_on(create_sqlite_pool())?;
+    task::block_on(create_db_tables(&pool));
+
+    println!("cool");
+    Ok(())
 }
