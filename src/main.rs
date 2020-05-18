572be01f575e0b8d03edfb8ffb1822209212aff2
Spyros Roum <spyrosr1@gmail.com>
2020-05-18 11:09:51 +0300

    Make main non-async

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,31 @@ mod schema;
 mod style;
 mod word;
 
-#[tokio::main]
-async fn main() {
-    let pool = db::get_pool().await;
-    db::create_tables(&pool).await;
-    let words = db::get_words(&pool).await;
+use std::thread;
+use std::sync::mpsc::channel;
+
+use tokio::runtime::Runtime;
+
+
+fn main() {
+    let (sx1, rx1) = channel();
+    let (sx2, rx2) = channel();
+
+    thread::spawn(move || {
+        let mut rt = Runtime::new().unwrap();
+
+        rt.block_on(async {
+            let pool = db::get_pool().await;
+            db::create_tables(&pool).await;
+            let words = db::get_words(&pool).await;
+
+            sx1.send(pool).unwrap();
+            sx2.send(words).unwrap();
+        });
+    });
+
+    let pool = rx1.recv().unwrap();
+    let words = rx2.recv().unwrap();
 
     gui::main(pool, words);
 }
